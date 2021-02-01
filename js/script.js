f37af11d5493e5a95e7ee0740fa9a9182116bee3
Nathaniel091 <nathaniel091@gmail.com>
2021-02-01 17:51:58 +0100

    completed the homepage

--- js/script.js
+++ js/script.js
@@ -5,20 +5,23 @@
 // - catch errors and out put it to the "warning"
 // - git commit "js library"
 
+	
+// ipifyAPI_URL = `https://geo.ipify.org/api/v1?apiKey=at_gbWW54UOrZHguQsCgMxbW0MKTeGR&ipAddress=`; // if the "ipAdress=" parameter is empty, the API request is made with the user's IP adress.
+
 // ===================================================================================================
 	// API details
 	// googleMapsEmbed
 	const googleMapsEmbed_API_KEY = `AIzaSyBL_nzHgULHBN9BACCmABwpR1IwWK27fvw`;
 	
-	// ipify details
-	// const ipify_API_KEY = `at_gbWW54UOrZHguQsCgMxbW0MKTeGRF`;
-	// const ipifyAPI_URL = `https://geo.ipify.org/api/v1?apiKey=${ipify_API_KEY}&ipAddress=`; // if the "ipAdress=" parameter is empty, the API request is made with the user's IP adress.
-	const ipify_API_KEY = `atgbWW54UOrZHguQsCgMxbW0MKTeGRF`;
-	const ipifyAPI_URL = `https://geo.ipify.org/api/v1?apiKey=${ipify_API_KEY}&ipAddress=`; // if the "ipAdress=" parameter is empty, the API request is made with the user's IP adress.
-
-	// Form and Search box details
+	// Form details
+	const submitButton = document.getElementById('submit-button')
 	const searchBox = document.getElementById('search-box');
-	const submitButton = document.getElementById('submit-button');
+	const searchValue = searchBox.value;
+
+	// ipify details
+	const ipify_API_KEY = `at_gbWW54UOrZHguQsCgMxbW0MKTeGRF`;
+	let ipAddress = "";
+
 
 	// Other details that will be updated with javascript
 	const IP_details = document.getElementById('IP-details');
@@ -43,12 +46,10 @@
 //===============================================================================================================
 
 
-// After page is loaded
+// After page is loaded =====================================================
 window.addEventListener('load', pageLoaded);
 
-// TEST CODE =====================================================
-function pageLoaded() {
-	
+function pageLoaded() {	
 	warning.textContent = warningText.pleaseWait;
 	// add loading animation
 	animation.classList.remove("d-none");
@@ -57,10 +58,18 @@ function pageLoaded() {
 		warning.style.cursor = 'progress';
 	}
 
+	// then call the fetchAPI() function
+	fetchAPI();
+}
+
+// function fetchAPI()
+function fetchAPI() {
+	// ipify API url
+	const ipifyAPI_URL = `https://geo.ipify.org/api/v1?apiKey=${ipify_API_KEY}&ipAddress=${ipAddress}`
+
 	fetch(ipifyAPI_URL)
 		.then(handleResponse)
 		.then((data) => {
-			console.log("data =>>", data)
 			let lat = data.location.lat;
 			let lng = data.location.lng;
 
@@ -79,79 +88,96 @@ function pageLoaded() {
 			ISP_details.textContent = `${data.isp}`;
 		})
 		.catch((error) => {
-			console.log("my ERROR =>>", error)
+			let statusCode = error.status;
+			// if error.status does not exist
+			if(typeof(error.status) !== "number") {
+				statusCode = error.code;
+			}
+
+			let message = error.messages;
+			// if error.messages does not exist
+			if(error.err){
+				message = error.err;
+				// .err is returned from the API sometimes
+			}
+			// if error.err does not exist
+			if(error.message) {
+				message = error.message;
+				// .message is browser in-built
+			}
 
 			// change the cursor
 			warning.style.cursor = 'help';
 			// stop the animation, change the text color to yellow & output the error and error message
 			animation.classList.add("d-none");
 			warning.classList.add("text-warning");
-			warning.textContent = `error! ${error.status} ${error.statusText}`;
-			warning.setAttribute("title", `MESSAGE: ${error.messages}`);
+			// warning.textContent = `error! ${statusCode} ${error.statusText} \n MESSAGE: ${message}`;
+			warning.innerHTML = `error! ${statusCode} ${error.statusText}. 
+							<br>
+			 				<span style="color:white;">message: '${message}'</span>`;
+			warning.style.lineHeight = '20px';
 		})
-		function handleResponse (response) {
-		 	let contentType = response.headers.get('content-type')
-		  	if (contentType.includes('application/json')) {
-		    		return handleJSONResponse(response)
-		  	} else if (contentType.includes('text/html')) {
-		    		return handleTextResponse(response)
-		  	} else {
-		    		// Other response types as necessary. I haven't found a need for them yet though.
-		    		// throw new Error(`Sorry, content-type ${contentType} not supported`)
-		    		Promise.reject(`Sorry, content-type ${contentType} not supported`)
-		  	}
-		}
-
-		function handleJSONResponse (response) {
-		  return response.json()
-		    .then(json => {
-		     	if (response.ok) {
-		        		return json
-		      	} else {
-		        		return Promise.reject(Object.assign({}, json, {
-		          		status: response.status,
-		          		statusText: response.statusText
-		        		}))
-		      	}
-		    })
-		}
-		function handleTextResponse (response) {
-		  	return response.text()
-		    .then(text => {
-		      	if (response.ok) {
-		        		return text
-		      	} else {
-			     	return Promise.reject({
-			     		status: response.status,
-			          	statusText: response.statusText,
-			          	err: text
-			        	})
-		      	}
-		    })
-		}
 }
-// TEST CODE ================================================
+
+// function handleResponse()
+function handleResponse(response) {
+ 	let contentType = response.headers.get('content-type')
+  	if (contentType.includes('application/json')) {
+    		return handleJSONResponse(response)
+  	} else if (contentType.includes('text/html')) {
+    		return handleTextResponse(response)
+  	} else {
+    		// Other response types as necessary. I haven't found a need for them yet though.
+    		// throw new Error(`Sorry, content-type ${contentType} not supported`)
+    		Promise.reject(`Sorry, content-type ${contentType} not supported`)
+  	}
+}
+
+// function handleJSONResponse()
+function handleJSONResponse(response) {
+	return response.json()
+    .then((json) => {
+     	if (response.ok) {
+        		return json
+      	} else {
+        		return Promise.reject(Object.assign({}, json, {
+          		status: response.status,
+          		statusText: response.statusText
+        		}));
+      	}
+    })
+}
+
+// function handleTextResponse()
+function handleTextResponse(response) {
+  	return response.text()
+    .then(text => {
+      	if (response.ok) {
+        		return text
+      	} else {
+	     	return Promise.reject({
+	     		status: response.status,
+	          	statusText: response.statusText,
+	          	err: text
+	        	})
+      	}
+    })
+}
 
 
+// When Form is submitted============================================================
+submitButton.addEventListener("click", function(event) {
+	event.preventDefault()
 
+	document.querySelector("#ip-address-container small").textContent = "IP ADDRESS";
 
-// On Form submission
-submitButton.addEventListener("click", afterFormSubmitted);
-
-// function after form submission
-function afterFormSubmitted(event) {
-	// prevent detault execution 
-	event.preventDefault();
-
+	// change "ipAddress" to the user's input (i.e searchValue)
 	const searchValue = searchBox.value;
+	ipAddress = searchValue
 
-	// if test has been passed
-	if (filterTestPassed(searchValue)) {
-		let warning = document.getElementById('warning');
-		fetchTheAPI();
-	}	
-}
-
+	// then call the "filterTestPassed" function to take over
+	filterTestPassed(searchValue)
+});
 
 // function filterTestPassed()
 function filterTestPassed(searchValue) {
@@ -159,68 +185,39 @@ function filterTestPassed(searchValue) {
 	// GOAL: "Anything else is a fail"
 
 	// regular expressions
-	const regexOnlyBeginningAndEndWhiteSpace = /([\s\b]|[\b\s])/g // matches white space only at the beginning & ending of the searchValue
+	const regexOnlyBeginningAndEndWhiteSpace = /([\s\b]|[\b\s])/g // matches all white spaces at the beginning & ending of the searchValue
 	const regexOnlyNumberAndPeriods = /[^0-9.]/g; // matches any character that is (or returns "true" if a character is) NOT a number(digit) or dot(.)
-
+	
 	// removes all whitespaces at the start & end of the searchValue
 	let modifiedSearchValue = searchValue.replace(regexOnlyBeginningAndEndWhiteSpace, "");
-	//====================================================================
 
-	
 	// Let warningText color turn to yellow
 	warning.classList.add("text-warning");
 
-	if(modifiedSearchValue === ""){
+	if(modifiedSearchValue === "") {
 		warning.textContent = warningText.emptyStringText;
 	} else if(regexOnlyNumberAndPeriods.test(modifiedSearchValue)) {
 		warning.textContent = warningText.defaultText;
-	}else {
+	} else {
 	 	// all tests has been passed, So?
 	 	warning.classList.remove("text-warning");
 	 	warning.textContent = warningText.pleaseWait;
 	 	animation.classList.remove("d-none")
-		return true; // do not delete
+		fetchAPI()
 	}
+};
+
+
+// current year for opyright
+currentYear()
+function currentYear() {
+	let currentYear = new Date().getFullYear()
+	let copyright = document.getElementById('copyright')
+	copyright.textContent += ` ${currentYear}`
 }
 
-
-//function fetchTheAPI()
-function fetchTheAPI() {
-	// declairing variables 
-	const searchValue = searchBox.value;
-	const ipifyAPI_URL2 = `https://geo.ipify.org/api/v1?apiKey=${ipify_API_KEY}&ipAddress=${searchValue}`;
-
-	fetch(ipifyAPI_URL2)
-		.then((response) => {
-			if(response.ok) {
-				return response.json()
-			}
-		})
-		.then((data) => {
-			console.log(data)
-
-			IP_details.textContent = `${data.ip}`;
-			location_details.textContent = `${data.location.city}, ${data.location.region}, ${data.location.country}`;
-			timezone_details.textContent = `${data.location.timezone}`;
-			ISP_details.textContent = `${data.isp}`;
-
-			let lat = data.location.lat;
-			let lng = data.location.lng;
-
-			// setURL for iframe
-			let map_IframeUrl = `https://www.google.com/maps/embed/v1/place?key=${googleMapsEmbed_API_KEY}&q=${lat},${lng}&center=${lat},${lng}&zoom=5&maptype=satellite`;
-			map_Iframe.setAttribute('src', map_IframeUrl)
-				
-			// display a new text & remove the loading animation
-			warning.textContent = warningText.disclaimerText;
-			animation.classList.add("d-none")
-		})
-		.catch((error) => {
-			warning.classList.add("text-warning")
-			warning.textContent = error;
-			console.error("Error Here --->> ", error)
-		});
-}
 // The End
+
+
 
 
